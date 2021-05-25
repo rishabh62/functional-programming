e7a7ab5f0795dc157eaf0ba43f27935f7581f0d4
Rishabh Gour <rgour@paypal.com>
2021-05-25 14:09:57 +0530

    added implementation for hasPath

--- haskell/hasPath.hs
+++ haskell/hasPath.hs
@@ -2,3 +2,14 @@
     problem statement : create a function hasPath that determines if a path from
                         one node to another exists within a directed graph.
 -}
+
+hasPath [] x y = x == y
+hasPath xs x y
+    | x == y = True
+    | otherwise =
+        let 
+            xs' = [(a, b) | (a, b) <- xs, a /= x]
+        in 
+            or [hasPath xs' b y | (a, b) <- xs, a == x]
+        
+-- Test code: hasPath [(1,2),(2,3),(3,2),(4,3),(4,5)] 1 3 -> True
